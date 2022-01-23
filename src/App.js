548e9e2ac5ad8c044b9cc04e6a5e91cd79ced8b8
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2022-01-23 14:15:04 +0330

    add header to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './containers/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="container">
+     <Header />
     </div>
   );
 }
---
+++ src/containers/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-sm bg-light">
+                <a class="navbar-brand text-dark" href="#">FakeShop</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
