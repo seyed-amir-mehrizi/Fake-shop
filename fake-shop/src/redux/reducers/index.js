6a7c0515b4b4c3bc1e06d5396c9ff95abb389ca4
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2022-01-23 13:41:29 +0330

    init the project again

--- fake-shop/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-
-import { combineReducers } from "redux";
-import { ProductReducer } from "./productReducer";
-
-const reducers = combineReducers({
-    allProducts : ProductReducer
-});
-
-export default reducers;
